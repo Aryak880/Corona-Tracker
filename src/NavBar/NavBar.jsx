394ce18a8e7661378fcc2c97521c6d11f760b1df
Aryak singh <aryaksinghchauhan@gmail.com>
2020-09-11 11:12:05 +0530

    second commit

--- src/NavBar/NavBar.jsx
+++ src/NavBar/NavBar.jsx
@@ -9,7 +9,7 @@ const NavBar = () => {
             <div className="row">                
 
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <img className="navbar-brand" src={image} style={{height: 60}}/>
+                <img className="navbar-brand" src={image} style={{height: 60}} alt="Logo"/>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
